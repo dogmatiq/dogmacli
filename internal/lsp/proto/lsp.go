07a6aa54c46ef6561dcfe1af403b1aaf51f89d33
James Harris <contact@jamesharris.id.au>
2023-05-08 06:30:33 +1000

    Use a `url.URL` to represent URIs.

--- internal/lsp/proto/lsp.go
+++ internal/lsp/proto/lsp.go
@@ -3,13 +3,37 @@ package proto
 import (
 	"bytes"
 	"encoding/json"
+	"net/url"
 )
 
 // URI is a string that represents a URI.
-type URI = string
+type URI struct {
+	url.URL
+}
+
+// MarshalJSON marshals the URI to JSON.
+func (u URI) MarshalJSON() ([]byte, error) {
+	return json.Marshal(u.String())
+}
+
+// UnmarshalJSON unmarshals the URI from JSON.
+func (u *URI) UnmarshalJSON(data []byte) error {
+	var s string
+	if err := unmarshal(data, &s); err != nil {
+		return err
+	}
+
+	p, err := url.Parse(s)
+	if err != nil {
+		return err
+	}
+
+	u.URL = *p
+	return nil
+}
 
 // DocumentURI is a string that represents a URI for a document.
-type DocumentURI = string
+type DocumentURI = URI
 
 func unmarshal(data []byte, v any) error {
 	dec := json.NewDecoder(bytes.NewReader(data))
